3d48f26ed5ad24bd99f5c81c0a2f7c2bbc2a2d99
andressfbermudez <andres201900@outlook.com>
2025-03-15 06:31:09 -0500

    Adding docs

--- BurbbleSort.java
+++ BurbbleSort.java
@@ -7,23 +7,26 @@ public class BurbbleSort {
         bubbleSort(numbers);
     }
 
+    // Algoritmo de ordenamiento de burbuja. Complejidad temporal de O(n^2)
     public static void bubbleSort(int[] array) {
-        int longitudArray = array.length;
+        int longitudArray = array.length; // Obtener la longitud del arreglo
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) { // Bucle que itera el array
 
-            boolean changes = false;
-            System.out.println(Arrays.toString(array));
+            boolean changes = false; // Para registrar si hay modificaciones en el array
+            System.out.println(Arrays.toString(array)); // Para monitorear los cambios
 
+            // Este bucle itera sobre el arreglo y va realizando los cambios necesarios
             for (int j = 0; j < (longitudArray - i - 1); j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
+                if (array[j] > array[j + 1]) { // Para intercambiar las posiciones
+                    int temp = array[j]; // Variable temporal que almacena la posicion del primer elemento a ordenar
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
-                    changes = true;
+                    changes = true; // Registrar los cambios
                 }
             }
 
+            // Si no hay cambios durante una ietracion completa del array quiere decir que ya esta ordenado
             if (!changes) {
                 break;
             }
